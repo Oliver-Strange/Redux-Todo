7d7de37290afd13e6177ff66c5ab9ad2784727b5
Oliver-Strange <preston.burton@gmail.com>
2019-05-21 14:49:49 -0700

    added some actions

---
+++ todo/src/store/actions/index.js
@@ -0,0 +1,29 @@
+export const UPDATE_TITLE = "UPDATE_TITLE";
+export const ADD_NEW_TODO = "ADD_NEW_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+
+export function updateTitle(newTitle) {
+  console.log(newTitle);
+  return {
+    type: UPDATE_TITLE,
+    payload: newTitle
+  };
+}
+
+// actions creators
+
+export const addNewTodo = todo => {
+  // action
+  return {
+    type: ADD_NEW_TODO,
+    payload: todo
+  };
+};
+
+export const toggleTodo = id => {
+  // console.log(id)
+  return {
+    type: TOGGLE_TODO,
+    payload: id
+  };
+};
