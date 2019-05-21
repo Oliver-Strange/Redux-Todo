97a407a263e8c75a1acc3e22ecee93371f5ee0ff
Oliver-Strange <preston.burton@gmail.com>
2019-05-21 14:21:08 -0700

    deleted app.js, changed src index.js to work with redux, added main container component

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import MainContainer from './components/MainContainer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(MainContainer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <MainContainer />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 
