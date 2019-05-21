74c8638f524b60d4fb4d33995f795761ad77caab
Oliver-Strange <preston.burton@gmail.com>
2019-05-21 14:47:41 -0700

    used friendslist title for todolist title

--- todo/src/components/Title.js
+++ todo/src/components/Title.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function Title() {
+import { updateTitle } from '../actions';
+
+class Title extends React.Component {
+  state = {
+    newTitleText: this.props.title,
+    editing: false
+  };
+
+  handleChanges = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  updateTitle = e => {
+    e.preventDefault();
+    this.props.updateTitle(this.state.newTitleText);
+    this.setState({ editing: false });
+  };
+
+  render() {
     return (
-        <h1>todo list title</h1>
+      <div>
+        {!this.state.editing ? (
+          <h1>
+            {this.props.title}{' '}
+            <i
+              className="far fa-edit"
+              onClick={() => this.setState({ editing: true })}
+            />
+          </h1>
+        ) : (
+          <div>
+            <input
+              className="title-input"
+              type="text"
+              name="newTitleText"
+              value={this.state.newTitleText}
+              onChange={this.handleChanges}
+            />
+            <button onClick={this.updateTitle}>Update title</button>
+          </div>
+        )}
+      </div>
     );
-} 
+  }
+}
 
-export default Title;
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    title: state.title
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { updateTitle } // same as { updateTitle: updateTitle }
+)(Title);
